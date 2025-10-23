083a6dd9e27b3f243b8611ad2c54cf87d233855d
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-10-23 12:52:23 +0200

    if and wehn number 2 done

---
+++ 40_JS_IF/5.js
@@ -0,0 +1,22 @@
+
+
+//Trafficlight = "green"
+let colours = (" red ")
+
+
+
+if (Trafficlight == "green") {
+    console.log("You may go");
+
+}
+    
+if (Trafficlight == "red") {
+    console.log("Stop!");
+
+}
+
+if (Trafficlight == "yellow") {
+    console.log(" Coution !");
+
+}
+
