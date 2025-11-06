6b8a5da954bbc15f00202209faf3066d35c4f854
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-11-06 11:39:12 +0100

    tre

--- 41_JS_Aufgaben Else/6.js
+++ 41_JS_Aufgaben Else/6.js
@@ -1,8 +1,8 @@
 let a = 11;
 
-if (aY = 11) {
-    
-    console.log(" The Number is big"); 
+if (a = 11) {
+
+    console.log(" The Number is big");
 } else {
     console.log(" The Number is small");
 
