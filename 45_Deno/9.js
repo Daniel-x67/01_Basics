a352e9d4b029629c9c574bbb9fbc45c5ab87c373
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-11-20 12:29:02 +0100

    45 Deno vidio 2 done

---
+++ 45_Deno/9.js
@@ -0,0 +1,9 @@
+let number1= prompt("Gib die erste Zahl ein ");
+let number2 = prompt(" Gib die zweite zahl ein ");
+
+
+let number1AsNumber = parseInt(number1);
+let number2AsNumber = parseInt(number2);
+
+let result = number1AsNumber + number2AsNumber;
+console.log(" Result " + result);
