d149a9375370023f90b79c6fca7dae71cce86e8f
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-11-20 11:54:07 +0100

    42 Aufgabe 1 done

--- 42_JS_If_Else/7.js
+++ 42_JS_If_Else/7.js
@@ -5,10 +5,10 @@ console.log(a);
 if (a < 5) {
     console.log("Die Zah ist kleiner als 5");  
     
-} else {
+} else if (a > 5) {
    
     console.log("Die Zahl ist größer als 5");
 
-} if (a == 5) {
+}  else {
 console.log(" Die Zahl ist genau 5 ")
 }   
