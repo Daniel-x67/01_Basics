a20f33c0868650aa7db07c615010a8c9adfe15f4
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-11-20 13:06:33 +0100

    45_Aufgabe_1_done

---
+++ 10.js
@@ -0,0 +1,11 @@
+let passwort = "67";
+
+let input = prompt(" Enter your Passwort ");
+
+if (input === passwort) {
+    console.log(" Access granted ");
+}
+if (input !== passwort) {
+    console.log("Access denied");
+    
+}
