6b83bbf0bb40ffc82441ac1f6dc81e9484df49ae
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-12-04 12:11:17 +0100

    deno Aufgabe 2 Done

---
+++ 45_Deno/11.js
@@ -0,0 +1,14 @@
+let number = 5;
+if (number > 0) {
+    console.log("The Number is Positiv ");
+} else if (number < 0) {
+
+    console.log("The Number is Negative");
+
+} else {
+    console.log("The Number ist Zero ");
+    
+}
+
+
+
