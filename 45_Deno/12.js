e6897760e09b4f1f66cd275451df02a96f77d474
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-12-04 12:58:43 +0100

    Deno Aufgabe 3 done

---
+++ 45_Deno/12.js
@@ -0,0 +1,37 @@
+let Number1 = Number(prompt("Enter your fist Number"));
+let Number2 = Number(prompt(" Enter your second Number "));
+let symbol = prompt("Enter your symbole( +,-,*,/ )");
+let result = Number1 + symbol + Number2;
+
+
+if (symbol == "+") {
+    
+let result = Number1 + Number2;
+
+console.log( Number1 + Number2)
+
+} else if (symbol == "-") {
+    
+    let result = Number1 - Number2
+
+    console.log(Number1 - Number2);
+    
+} else if (symbol == "*") {
+    
+    let result = Number1 + Number2
+    
+    console.log(Number1 * Number2);
+    
+} else {
+
+    let result = Number1 / Number2
+    
+    console.log(Number1 / Number2);
+    
+}
+
+ 
+
+
+
+
