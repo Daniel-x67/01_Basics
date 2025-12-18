08bb843c47012076424595af50f812918ef19cff
Daniel-x6 <daniel.innerkofler@pcn.team>
2025-12-18 12:51:49 +0100

    45_Deno task Drinks done

---
+++ 45_Deno/15.js
@@ -0,0 +1,11 @@
+console.log(" What drink do you want ? ");
+let awnser = prompt(" Enter your drink ( Wasser, Bier , Cola, Tee ) ");
+if (awnser == "Wasser") {
+    console.log(" Thanks for choosing Wasser. ");
+} else if (awnser == "Bier") {
+    console.log(" Thanks for choosing Bier. ");
+} else if (awnser == "Cola") {
+    console.log(" Thanks for choosing Cola. ");
+} else {
+    console.log(" Thanks for choosing Tee. ");
+}
