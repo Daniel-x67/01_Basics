3a47fdfb7dab2c3a3c115723cb66dce2292c1c95
Daniel-x6 <daniel.innerkofler@pcn.team>
2026-01-29 12:16:58 +0100

    60_Loop_5_Done

---
+++ 60_Loops/26.js
@@ -0,0 +1,10 @@
+
+let a =prompt("Enter your Number:")
+let b = prompt("Enter your second Number:")
+a = Number(a);
+b = Number(b);
+for (let i = a; i <= b; i++) {
+   
+}
+let sum =(a + b);
+console.log(sum);
